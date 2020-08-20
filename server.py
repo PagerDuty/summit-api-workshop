c1d05c3d375164306b155b89b468af1f2d6fbe53
Brett Willemsen <bwillemsen@pagerduty.com>
2020-08-20 15:09:00 -0400

    Add dotenv support.

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+from os import environ as ENV
 from flask import Flask
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route('/hello-world', methods=['GET'])
-def slash_command():
-    return "Hello, World."
+def hello_world_route():
+    return f"Hello, World. {ENV.get('TEST_KEY')}"
 
 if __name__ == "__main__":
     app.run(debug=True)
