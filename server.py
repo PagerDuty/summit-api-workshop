0b9d53aa45b8bf6929fa5d4abe3d4817e3320ed8
Brett Willemsen <bwillemsen@pagerduty.com>
2020-08-20 15:34:42 -0400

    Add pdpyras.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from os import environ as ENV
 from flask import Flask
 from dotenv import load_dotenv
+from pdpyras import APISession
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,6 +9,16 @@ app = Flask(__name__)
 @app.route('/hello-world', methods=['GET'])
 def hello_world_route():
     return f"Hello, World. {ENV.get('TEST_KEY')}"
+
+@app.route('/test-pdpyras', methods=['GET'])
+def test_pdpyras_route():
+    session = APISession(ENV.get('PAGERDUTY_REST_API_KEY'))
+
+    # Using requests.Session.get:
+    response = session.get('/users?total=true')
+    if response.ok:
+        total_users = response.json()['total']
+        return f"Account has {total_users} users."
 
 if __name__ == "__main__":
     app.run(debug=True)
