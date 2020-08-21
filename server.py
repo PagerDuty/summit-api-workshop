4e4ec0048ac98350397f5495ac24fe0673281eab
Brett Willemsen <bwillemsen@pagerduty.com>
2020-08-21 15:57:40 -0400

    Add endpoint that creates a new service and another for a new EP.

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from os import environ as ENV
 from flask import Flask
 from dotenv import load_dotenv
-from pdpyras import APISession
+from pdpyras import APISession, PDClientError
 
 load_dotenv()
 app = Flask(__name__)
@@ -19,6 +19,40 @@ def test_pdpyras_route():
     if response.ok:
         total_users = response.json()['total']
         return f"Account has {total_users} users."
+
+@app.route('/get-escalation-policy', methods=['GET'])
+def test_get_ep_route():
+    session = APISession(ENV.get('PAGERDUTY_REST_API_KEY'))
+    try:
+        ep = session.rget(
+            '/escalation_policies',
+            params={'query': 'Default'})
+        return f"EP {ep}."
+    except PDClientError as e:
+        return e.msg
+
+@app.route('/create-service', methods=['GET'])
+def test_create_service_route():
+    session = APISession(ENV.get('PAGERDUTY_REST_API_KEY'))
+
+    # Using requests.Session.get:
+    try:
+        service = session.rpost(
+            '/services',
+            json={
+                'name': '#PDSummit Twitter',
+                'type': 'service',
+                'description': 'hey',
+                "escalation_policy": {
+                    "id": "P7U5VP4",
+                    "type": "escalation_policy_reference"
+                },
+                "alert_creation": "create_alerts_and_incidents"
+            })
+        return f"Service {service}."
+    except PDClientError as e:
+        return e.msg
+
 
 if __name__ == "__main__":
     app.run(debug=True)
